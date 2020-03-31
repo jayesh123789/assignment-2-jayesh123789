84d18661a3a4f8b883594a79ad212bba7f29f731
Harshit Sachan <harshit.sachan_cs18@gla.ac.in>
2020-03-31 19:40:56 +0530

    problem 1 shows :- print all left children and count nodes

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,30 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
-// executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
-public class MyMain {
+import problem1.mybst.MyBinarySearchTree;
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+        MyBinarySearchTree m = new MyBinarySearchTree();
+        Methods mthds = new Methods();
+
+        //setting up the root
+        m.setRoot();
+        System.out.println("Root set Successfully value :" + m.getRoot().getData());
+
+
+        //Setting up the bst
+        for (int i = 0; i < 4; i++)
+        {
+            m.insert(m.getRoot());
+        }
+
+        // Verification of statement "root element occours first in pre-order and last in post-order  "
+
+        mthds.verify_A(m.getRoot());
+
+        //verification of statement "Both the traversal will give same  element at the mid position for odd number of nodes."
+        System.out.println(mthds.verify_B(m.getRoot()));
+    }
 }
